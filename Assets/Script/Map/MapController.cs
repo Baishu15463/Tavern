9f1d65ec1a3fd0399e3e78478ca6e6712831c5a8
Baishu <1546387481@qq.com>
2026-09-23 20:58:01 +0800

    放置物体，设置层级，添加自动转换层级脚本

--- Assets/Script/Map/MapController.cs
+++ Assets/Script/Map/MapController.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MapController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Tooltip("越小越靠前，通常设为 100 或 1000")]
+    public int precision = 100;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        // 核心公式：Order in Layer = -Y坐标 * 精度
+        // Y 越小（越靠下），-Y 越大，Order 越大，渲染越靠前
+        spriteRenderer.sortingOrder = Mathf.RoundToInt(-transform.position.y * precision);
     }
 }
